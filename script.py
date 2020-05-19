1504d8ff40420d6d990e7596f486f4bad1e97a43
matheus <dacachmatheus@gmail.com>
2020-05-19 16:30:42 -0300

    add vscode snippets

--- script.py
+++ script.py
@@ -6,14 +6,19 @@ def run(cmd: str):
     subprocess.run(cmd.split(" ")) 
 
 
-alacritty = 'ln -s -f /home/matheus/.dotfiles/.alacrity.yml /home/matheus/.alacritty.yml' 
+alacritty = 'ln -s -f /home/matheus/.dotfiles/.alacritty.yml /home/matheus/.alacritty.yml' 
 vim = 'ln -s -f /home/matheus/.dotfiles/.vimrc /home/matheus/.vimrc' 
 zsh = 'ln -s -f /home/matheus/.dotfiles/.zshrc /home/matheus/.zshrc' 
 tmux = 'ln -s -f /home/matheus/.dotfiles/.tmux.conf /home/matheus/.tmux.conf'
 redshift = 'ln -s -f /home/matheus/.dotfiles/.redshift.conf /home/matheus/.config/redshift.conf' 
+vscode = 'ln -s -f /home/matheus/.dotfiles/vscode/settings.json /home/matheus/.config/Code/User/settings.json' 
+vscode_snippets = 'ln -s -f /home/matheus/.dotfiles/vscode/snippets/cpp.json /home/matheus/.config/Code/User/snippets/cpp.json' 
+
 
 run(alacritty) 
 run(vim)
 run(zsh)
 run(tmux)
 run(redshift) 
+run(vscode)
+run(vscode_snippets)
